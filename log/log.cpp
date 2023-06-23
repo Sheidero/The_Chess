0012a851805bad45f6d416f413e39fd9cff3909a
DenY00s <noviko8d@mail.ru>
2023-06-23 13:16:10 +0200

    added logger

---
+++ log/log.cpp
@@ -0,0 +1,51 @@
+#include "log.hpp"
+
+
+void Log::write_log(const std::string& type_str, const std::string& log_msg) {
+	time_t now = time(0);
+	tm* ltm = localtime(&now);
+	char buffer[80];
+	strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", ltm);
+	m_out << "[" << buffer << "] " << "[" << type_str << "] " << log_msg << std::endl;
+}
+Log::Log()
+{
+	std::cout << "constr";
+}
+void Log::SetLogLevel(LogLevel loglevel)
+{
+	m_loglevel = loglevel;
+}
+void Log::SetLogPath(const std::string& path)
+{
+	m_out.open(path);
+}
+void Log::Write(LogLevel type, const std::string& msg)
+{
+	switch (type)
+	{
+	case LogLevel::DEBUG:
+#ifdef _DEBUG
+		write_log("DEB", msg);
+#endif // _DEBUG
+
+		break;
+	case LogLevel::INFO:
+		write_log("INFO", msg);
+		break;
+	case LogLevel::ERROR:
+		write_log("ERR", msg);
+		break;
+	case LogLevel::WARN:
+		write_log("WARN", msg);
+		break;
+	default:
+		break;
+	}
+}
+Log::~Log()
+{
+	std::cout << "destr";
+}
+std::ofstream Log::m_out;
+LogLevel Log::m_loglevel;
---
+++ log/log.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+enum class LogLevel
+{
+	DEBUG,
+	INFO,
+	ERROR,
+	WARN
+};
+class Log
+{
+private:
+	static std::ofstream m_out;
+	static LogLevel m_loglevel;
+	static void write_log(const std::string& type_str, const std::string& log_msg);
+		
+	
+public:
+	Log();
+	
+	static void SetLogLevel(LogLevel loglevel);
+	
+	static void SetLogPath(const std::string& path);
+	
+	static void Write(LogLevel type, const std::string& msg);
+	
+	
+	~Log();
+	
+};
